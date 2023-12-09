f2b3807e0c6158614c8a794ed38ea461fb9b6b88
virvaldev <valtteri.virtanen.dev@gmail.com>
2023-12-09 14:24:04 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const App: React.FC = () => {
+  // code
+
+  return (
+    <div className="App">
+      <h1>React App</h1>
+    </div>
+  )
+}
+
+export default App;
